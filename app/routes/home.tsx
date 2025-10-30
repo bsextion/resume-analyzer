02c00b0d16c8ed022e8db87f57bcb89cecc3a56a
bsextion <29663895+bsextion@users.noreply.github.com>
2025-10-30 00:09:27 -0500

    added homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,20 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resume Analyzer" },
+    { name: "description", content: "Resume Analyzer for job hunting!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <section className="main-section">
+          <div className="page-heading">
+              <h1>Job Application Tracker & Smart Resume Builder</h1>
+              <h2>Track and review your job submissions & receive AI feedback!</h2>
+          </div>
+      </section>
+
+  </main>;
 }
